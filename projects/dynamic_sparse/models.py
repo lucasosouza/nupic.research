c3a2fddf03343d29a844c29ca668639c8e752851
lucasosouza <lucasosouza@gmail.com>
2019-07-18 18:41:38 -0700

    added structure hebbian learning, added early stop for structure learning

--- projects/dynamic_sparse/models.py
+++ projects/dynamic_sparse/models.py
@@ -48,6 +48,9 @@ class BaseModel:
             weight_prune_perc=0,
             grad_prune_perc=0,
             test_noise=False,
+            percent_on=0.3,
+            boost_strength=1.4,
+            boost_strength_factor=0.7,            
         )
         defaults.update(config or {})
         self.__dict__.update(defaults)
@@ -64,8 +67,9 @@ class BaseModel:
                 self.network.parameters(), lr=self.learning_rate
             )
         elif self.optim_alg == "SGD":
+            # added weight decay
             self.optimizer = optim.SGD(
-                self.network.parameters(), lr=self.learning_rate, momentum=self.momentum
+                self.network.parameters(), lr=self.learning_rate, momentum=self.momentum, weight_decay=2e-4
             )
 
         # add a learning rate scheduler
@@ -526,3 +530,207 @@ class DSNN(SparseModel):
         if self.debug_sparse:
             for idx, m in enumerate(self.masks):
                 self.log["mask_sizes_l" + str(idx)] = torch.sum(m).item()
+
+
+class DSNNHeb(SparseModel):
+    """
+    Dynamically sparse neural networks.
+    Our improved version of SET
+    """
+
+    def setup(self):
+        super(DSNNHeb, self).setup()
+        self.added_synapses = [None for m in self.masks]
+        self.last_gradients = [None for m in self.masks]
+
+        # initializae sign to 1 if to be flipped later
+        self.prune_grad_sign = -1
+        if self.flip:
+            self.prune_grad_sign = 1
+            self.flip_epoch = 30
+
+        # initialize hebbian learning
+        self.network.hebbian_learning = True
+
+        # add specific defaults
+        new_defaults = (dict(
+            pruning_active=True,
+            # pruning_early_stop=True,
+            pruning_early_stop_threshold=0.02,
+        ))
+        new_defaults = {k:v for k,v in new_defaults.items() if k not in self.__dict__ }
+        self.__dict__.update(new_defaults)
+
+    def _post_epoch_updates(self, dataset=None):
+        super(DSNNHeb, self)._post_epoch_updates(dataset)
+
+        # flip at a fixed interval
+        if self.flip:
+            if self.current_epoch == self.flip_epoch and self.prune_grad_sign == 1:
+                self.prune_grad_sign = -1
+
+        # zero out correlations
+        self.network.correlations = []
+
+        # update only when learning rate updates
+        # froze this for now
+        # if self.current_epoch in self.lr_milestones:
+        #     # decay pruning interval, inversely proportional with learning rate
+        #     self.pruning_interval = max(self.pruning_interval,
+        #         int((self.pruning_interval * (1/self.lr_gamma))/3))
+
+    def _run_one_pass(self, loader, train, noise=False):
+        """TODO: reimplement by calling super and passing a hook"""
+        epoch_loss = 0
+        epoch_correct = 0
+        for inputs, targets in loader:
+            # setup for training
+            inputs = inputs.to(self.device)
+            targets = targets.to(self.device)
+            self.optimizer.zero_grad()
+
+            # forward + backward + optimize
+            with torch.set_grad_enabled(train):
+                outputs = self.network(inputs)
+                _, preds = torch.max(outputs, 1)
+                epoch_correct += torch.sum(targets == preds).item()
+                loss = self.loss_func(outputs, targets)
+                if train:
+                    loss.backward()
+                    # zero the gradients for dead connections
+                    for idx, (mask, m) in enumerate(
+                        zip(self.masks, self.sparse_modules)
+                    ):
+                        m.weight.grad *= mask
+                        # save gradients before any operation
+                        # TODO: will need to integrate over several epochs later
+                        self.last_gradients[idx] = m.weight.grad
+
+                    self.optimizer.step()
+
+            # keep track of loss and accuracy
+            epoch_loss += loss.item() * inputs.size(0)
+
+        # store loss and acc at each pass
+        loss = epoch_loss / len(loader.dataset)
+        acc = epoch_correct / len(loader.dataset)
+        if train:
+            self.log["train_loss"] = loss
+            self.log["train_acc"] = acc
+        else:
+            if noise:
+                self.log["noise_loss"] = loss
+                self.log["noise_acc"] = acc
+            else:
+                self.log["val_loss"] = loss
+                self.log["val_acc"] = acc
+
+        # add monitoring of weights
+        if train and self.debug_weights:
+            self._log_weights()
+
+        # add monitoring of sparse levels
+        if train and self.debug_sparse:
+            self._log_sparse_levels()
+
+        # dynamically decide pruning interval
+        if train:
+            # no dynamic interval at this moment
+            # if self.current_epoch % self.pruning_interval == 0:
+            self.reinitialize_weights()
+
+    def prune(self, weight, grad, num_params, corr, idx=0):
+        """
+        Calculate new weight based on SET approach weight vectorized version
+        aimed at keeping the mask with the similar level of sparsity.
+
+        Changes:
+        - higher zeta
+        - two masks: one based on weights, another based on gradients
+
+        Have access to correlation 
+        Error added contiguous to fix
+        RuntimeError: invalid argument 2: view size is not compatible with input tensor's size and stride (at least one dimension spans across two contiguous subspaces). Call .contiguous() before .view(). at /pytorch/aten/src/THC/generic/THCTensor.cpp:209
+        """
+        with torch.no_grad():
+
+            # calculate weight mask
+            zeta = self.weight_prune_perc
+            weight_pos = weight[weight > 0]
+            pos_threshold, _ = torch.kthvalue(
+                weight_pos, max(int(zeta * len(weight_pos)), 1)
+            )
+            weight_neg = weight[weight < 0]
+            neg_threshold, _ = torch.kthvalue(
+                weight_neg, max(int((1 - zeta) * len(weight_neg)), 1)
+            )
+            weight_keep_mask = (weight >= pos_threshold) | (weight <= neg_threshold)
+            weight_keep_mask.to(self.device)
+            self.log["weight_keep_mask_l" + str(idx)] = torch.sum(
+                weight_keep_mask
+            ).item()
+
+            # no gradient mask, just a keep mask
+            keep_mask = weight_keep_mask
+
+            # calculate number of parameters to add
+            num_add = num_params - torch.sum(keep_mask).item()
+            self.log["missing_weights_l" + str(idx)] = num_add            
+            # transpose to fit the weights
+            corr = corr.t()
+            # remove the ones which will already be kept
+            corr *= (keep_mask == 0).float()
+            # get kth value, based on how many weights to add, and calculate mask
+            kth = int(np.prod(corr.shape) - num_add)
+            # contiguous()
+            corr_threshold, _ = torch.kthvalue(corr.contiguous().view(-1), kth)
+            add_mask = (corr > corr_threshold).to(self.device)
+
+            new_mask = keep_mask | add_mask
+            self.log["added_synapses_l" + str(idx)] = torch.sum(add_mask).item()
+
+        # track added connections
+        return new_mask, keep_mask, add_mask
+
+    def reinitialize_weights(self):
+        """Reinitialize weights."""
+        # only run if still learning. method will be called but do nothing
+        if self.pruning_active:
+
+            for idx, (m, grad, corr) in enumerate(zip(self.sparse_modules, self.last_gradients, self.network.correlations)):
+                new_mask, keep_mask, new_synapses = self.prune(
+                    m.weight.clone().detach(), grad, self.num_params[idx], corr, idx=idx
+                )
+                with torch.no_grad():
+                    self.masks[idx] = new_mask.float()
+                    m.weight.data *= keep_mask.float()
+
+                    # keep track of added synapes
+                    survival_ratios = []
+
+                    # count how many synapses from last round have survived
+                    if self.added_synapses[idx] is not None:
+                        total_added = torch.sum(self.added_synapses[idx]).item()
+                        surviving = torch.sum(
+                            self.added_synapses[idx] & keep_mask
+                        ).item()
+                        if total_added:
+                            survival_ratio = (surviving / total_added)
+                            survival_ratios.append(survival_ratio)
+                            # log if in debug sparse mode
+                            if self.debug_sparse:
+                                self.log["surviving_synapses_l" + str(idx)] = survival_ratio
+
+                    # keep track of new synapses to count surviving on next round
+                    self.added_synapses[idx] = new_synapses
+
+            # early stop
+            if self.pruning_early_stop:
+                if np.mean(survival_ratios) < self.pruning_early_stop_threshold:
+                    self.pruning_active = False 
+
+            # keep track of mask sizes when debugging
+            if self.debug_sparse:
+                for idx, m in enumerate(self.masks):
+                    self.log["mask_sizes_l" + str(idx)] = torch.sum(m).item()
+
--- projects/dynamic_sparse/networks.py
+++ projects/dynamic_sparse/networks.py
@@ -25,7 +25,9 @@ import torch
 from torch import nn
 from torchvision import models
 
-from nupic.torch.modules import Flatten, KWinners2d
+from nupic.torch.modules import Flatten, KWinners2d, KWinners
+
+from collections import defaultdict
 
 
 def vgg19_bn(config):
@@ -80,6 +82,7 @@ class VGG19(nn.Module):
         super(VGG19, self).__init__()
 
         defaults = dict(
+            device='gpu',
             input_size=784,
             num_classes=10,
             hidden_sizes=[4000, 1000, 4000],
@@ -88,6 +91,8 @@ class VGG19(nn.Module):
             bias=False,
             init_weights=True,
             kwinners=False,
+            percent_on=0.3,
+
         )
         defaults.update(config or {})
         self.__dict__.update(defaults)
@@ -121,7 +126,7 @@ class VGG19(nn.Module):
             *self._conv_block(512, 512, pool=True),  # 1x1
         ]
         layers.append(Flatten())
-        layers.append(nn.Linear(512, config["num_classes"]))
+        layers.append(nn.Linear(512, self.num_classes))
         self.classifier = nn.Sequential(*layers)
 
         if self.init_weights:
@@ -147,6 +152,117 @@ class VGG19(nn.Module):
 
     def forward(self, x):
         return self.classifier(x)
+
+    def _initialize_weights(self):
+        for m in self.classifier.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2.0 / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+class VGG19Small(nn.Module):
+    def __init__(self, config=None):
+        super(VGG19Small, self).__init__()
+
+        defaults = dict(
+            device='gpu',
+            input_size=784,
+            num_classes=10,
+            hidden_sizes=[4000, 1000, 4000],
+            batch_norm=False,
+            dropout=0.3,
+            bias=False,
+            init_weights=True,
+            kwinners=False,
+            percent_on=0.3,
+            boost_strength=1.4,
+            boost_strength_factor=0.7,            
+        )
+        defaults.update(config or {})
+        self.__dict__.update(defaults)
+        self.device = torch.device(self.device)
+
+        # define if kwinners or regular network
+        if self.kwinners:
+            self.pool_func = lambda: nn.AvgPool2d(kernel_size=2, stride=2)
+            self.nonlinear_func = self._kwinners
+        else:
+            self.pool_func = lambda: nn.MaxPool2d(kernel_size=2, stride=2)
+            self.nonlinear_func = lambda fout: nn.ReLU()
+
+        # initialize network
+        layers = [
+            *self._conv_block(3, 64, pool=True),     # 16x16
+            *self._conv_block(64, 64, pool=True),     # 8x8
+            *self._conv_block(64, 128, pool=True),   # 4x4
+            *self._conv_block(128, 256, pool=True),  # 2x2
+            *self._conv_block(256, 512, pool=True),  # 1x1
+        ]
+        layers.append(Flatten())
+        layers.append(nn.Linear(512, self.num_classes))
+        self.classifier = nn.Sequential(*layers)
+
+        # track the activations
+        # should reset at the end of each round, need to find that, can be done in the model
+        self.correlations = []
+
+        if self.init_weights:
+            self._initialize_weights()
+
+    def _kwinners(self, fout):
+        return KWinners2d(
+            channels=fout,
+            percent_on=self.percent_on,
+            boost_strength=self.boost_strength,
+            boost_strength_factor=self.boost_strength_factor,
+        )
+
+    def _conv_block(self, fin, fout, pool=False):
+        block = [
+            nn.Conv2d(fin, fout, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(fout),
+            self.nonlinear_func(fout),
+        ]
+        if pool:
+            block.append(self.pool_func())
+        return block
+
+    def forward(self, x):
+        idx = 0
+        for layer in self.classifier:
+            if not (isinstance(layer, Flatten) or isinstance(layer, nn.AvgPool2d) or isinstance(layer, nn.BatchNorm2d)):                
+                x = layer(x)
+                print(layer.__class__)
+                if idx == 0:
+                    # clone and detach x
+                    # sum over batch, interested in correlation between neurons
+                    prev_act = torch.sum(x.clone().detach(), dim=0).view(-1)
+                # from the second layer
+                else:
+                    curr_act = torch.sum(x.clone().detach(), dim=0).view(-1)
+                    # only append correlation if it is a layer with weights
+                    if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+                        curr_act = torch.sum(x.clone().detach(), dim=0).view(-1)
+                        # get the outer product, square root to control growth
+                        outer_product = torch.sqrt(torch.ger(prev_act, curr_act))
+                        # if non existing, append
+                        if idx > len(self.correlations):
+                            self.correlations.append(outer_product)
+                        else:
+                            self.correlations[idx-1] += outer_product
+                    prev_act = curr_act
+                idx += 1
+
+        return x
 
     def _initialize_weights(self):
         for m in self.classifier.modules():
@@ -233,6 +349,115 @@ class MLP(nn.Module):
 
     def forward(self, x):
         return self.classifier(x.view(-1, self.input_size))
+
+    def _initialize_weights(self, bias):
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm1d):
+                nn.init.constant_(m.weight, 1)
+                if bias:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                if bias:
+                    nn.init.constant_(m.bias, 0)
+
+
+
+
+class MLPHeb(nn.Module):
+    """
+    Simple 3 hidden layers + output MLPHeb, similar to one used in SET Paper.
+    """
+
+    def __init__(self, config=None):
+        super(MLPHeb, self).__init__()
+
+        defaults = dict(
+            input_size=784,
+            num_classes=10,
+            hidden_sizes=[1000, 1000, 1000],
+            batch_norm=False,
+            dropout=False,
+            bias=False,
+            init_weights=True,
+            hebbian_learning=False
+        )
+        defaults.update(config or {})
+        self.__dict__.update(defaults)
+        self.device = torch.device(self.device)
+
+        if self.kwinners:
+            self.activation_func = self._kwinners
+        else:
+            self.activation_func = lambda fout: nn.ReLU()
+
+        # hidden layers
+        layers = [
+            *self._linear_block(self.input_size, self.hidden_sizes[0]),
+            *self._linear_block(self.hidden_sizes[0], self.hidden_sizes[1]),
+            *self._linear_block(self.hidden_sizes[1], self.hidden_sizes[2]),
+        ]
+        # output layer
+        layers.append(nn.Linear(self.hidden_sizes[2], self.num_classes, bias=self.bias))
+
+        # classifier (*redundancy on layers to facilitate traversing)
+        self.layers = layers
+        self.classifier = nn.Sequential(*layers)
+
+        if self.init_weights:
+            self._initialize_weights(self.bias)
+
+        # track correlations
+        self.correlations = []     
+
+    def _kwinners(self, fout):
+        return KWinners(
+            n=fout,
+            percent_on=self.percent_on,
+            boost_strength=self.boost_strength,
+            boost_strength_factor=self.boost_strength_factor,
+        )
+
+    def _linear_block(self, fin, fout):
+        block = [nn.Linear(fin, fout, bias=self.bias), self.activation_func(fout)]
+        if self.batch_norm:
+            block.append(nn.BatchNorm1d(fout))
+        if self.dropout:
+            block.append(nn.Dropout(p=self.dropout))
+
+        return block
+
+    def _has_activation(self, idx, layer):
+        return idx == len(self.layers)-1 or isinstance(layer, nn.ReLU) or isinstance(layer, KWinners)
+
+    def forward(self, x):
+        """ A smarter way of building it """
+
+        x = x.view(-1, self.input_size) # resiaze if needed, eg mnist
+        prev_act = (x > 0).detach().float()
+        idx_activation = 0
+        for idx_layer, layer in enumerate(self.layers):
+            # do the forward calculation normally
+            x = layer(x)
+            if self.hebbian_learning:
+                n_samples = x.shape[0]
+                if self._has_activation(idx_layer, layer):
+                    with torch.no_grad():
+                        curr_act = (x > 0).detach().float()
+                        # add outer product to the correlations, per sample
+                        for s in range(n_samples):
+                            outer = torch.ger(prev_act[s], curr_act[s])
+                            if idx_activation+1 > len(self.correlations):
+                                self.correlations.append(outer)
+                            else:
+                                self.correlations[idx_activation] += outer
+
+                        # reassing to the next
+                        prev_act = curr_act
+                        # move to next activation
+                        idx_activation += 1
+
+        return x
 
     def _initialize_weights(self, bias):
         for m in self.modules():
